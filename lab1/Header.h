b4034cf2c8d22a41c9729ef35a4f61987e7fa440
Karina Malij <karinamalij0303@gmail.com>
2026-03-17 17:11:02 +0200

    .h

--- lab1/Header.h
+++ lab1/Header.h
@@ -1,15 +1,21 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include <cctype>
 #include <map>
 #include <cmath>
+#include<set>
 using namespace std;
 
 string readText(const string& filename);
 string cleanText(const string& text);
-//string removeSpaces(const string& text);
+string removeSpaces(const string& text);
 
 double calculateH1(const string& text);
-//double calculateH2(const string& text);
+double calculateH2(const string& text);
+void printLetters(const string& text, ofstream& out);
+void printBigram(const string& text, ofstream& out);
