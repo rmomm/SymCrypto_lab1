89f9e7244d4986bb41f0cdfc5a1f70c325ff0726
Karina Malij <karinamalij0303@gmail.com>
2026-03-17 17:12:00 +0200

    entropy

--- lab1/entropy.cpp
+++ lab1/entropy.cpp
@@ -20,3 +20,30 @@ double calculateH1(const string& text) {
 
     return H1;
 }
+
+double calculateH2(const string& text) {
+    map<string, int> freq;
+    int total = 0;
+
+    for (size_t i = 0; i + 1 < text.size(); i++) {
+        if (text[i] == ' ' || text[i + 1] == ' ') {
+            continue;
+        }
+
+        string bigram;
+        bigram += text[i];
+        bigram += text[i + 1];
+
+        freq[bigram]++;
+        total++;
+    }
+
+    double H2 = 0.0;
+
+    for (auto& pair : freq) {
+        double p = (double)pair.second / total;
+        H2 -= p * log2(p);
+    }
+
+    return H2 / 2.0;
+}
