c7c5e97ba51435242dbf8139b69a9a0322874b77
Karina Malij <karinamalij0303@gmail.com>
2026-03-17 19:44:15 +0200

    main'

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -3,16 +3,32 @@
 int main()
 {
     string text = readText("text.txt");
-
     string cleaned = cleanText(text);
+    string noSpaces = removeSpaces(cleaned);
 
-    cout << "Original length: " << text.size() << endl;
-    cout << "Cleaned length: " << cleaned.size() << endl;
+    ofstream out("result.txt");
 
     double H1 = calculateH1(cleaned);
+    double H2 = calculateH2(cleaned);
 
-    cout << "H1 = " << H1 << endl;
+    double H1_ns = calculateH1(noSpaces);
+    double H2_ns = calculateH2(noSpaces);
 
+    out << "Ç ÏÐÎÁ²ËÀÌÈ \n";
+    out << "H1 = " << H1 << "\n";
+    out << "H2 = " << H2 << "\n";
+
+    printLetters(cleaned, out);
+    printBigram(cleaned, out);
+
+    out << "\n ÁÅÇ ÏÐÎÁ²Ë²Â \n";
+    out << "H1 = " << H1_ns << "\n";
+    out << "H2 = " << H2_ns << "\n";
+
+    printLetters(noSpaces, out);
+    printBigram(noSpaces, out);
+
+    out.close();
 
     return 0;
 }
